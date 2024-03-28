1976b85af7c0d6fd7e3b816e91a3f57e65effbb4
coding655 <codingmaster655@gmail.com>
2024-03-28 10:43:10 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById('btn').addEventListener('click', function(){
+    /* email */
+    const emailField = document.getElementById('email-field');
+    const email = emailField.value;
+    /* pass */
+    const passField = document.getElementById('pass-field');
+    const pass = passField.value;
+    if(email === 'rahulkadir@gmail.com' && pass === 'password'){
+        window.location.href = 'index.html';
+    }
+    else{
+        alert('Wrong password');
+    }
+})
